3a13a2e99324cd3945cc9eb60482ab8cab8b13bf
T.Serck <timserck@gmail.com>
2025-09-25 19:40:59 +0200

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "leaflet/dist/leaflet.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
